1f871102bb52da321ea8986266385a6ecb8054fb
Student <Student@193.107.34.206>
2018-03-06 11:03:01 +0100

    abc

--- Blog/src/blog/Blog.java
+++ Blog/src/blog/Blog.java
@@ -15,6 +15,7 @@ public class Blog {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int a=5;
         // TODO code application logic here
     }
     
